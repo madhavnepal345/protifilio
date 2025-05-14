4b6b1d7da549cf7ab49abb6987561a8334ae56fc
madhav <madhavnepal916@gmail.com>
2025-05-14 15:02:04 +0545

    added some models in the database

--- backend/protifilio/details/admin.py
+++ backend/protifilio/details/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import HomeContent,Project,About
+
+admin.site.register(HomeContent)
+admin.site.register(Project)
+admin.site.register(About)
 
 # Register your models here.
