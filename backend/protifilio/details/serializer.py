a0500577b9b16617b33c4664087555e01c8b46d8
madhav <madhavnepal916@gmail.com>
2025-05-14 16:34:57 +0545

    added required serializer class

--- backend/protifilio/details/serializer.py
+++ backend/protifilio/details/serializer.py
@@ -1,2 +1,19 @@
 from rest_framework import serializers
 from .models import HomeContent,About,Project
+
+
+class HomeContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=HomeContent
+        fields="__all__"
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=About
+        fields="__all__"
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields="__all__"
+        
