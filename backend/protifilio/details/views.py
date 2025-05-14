5128b9daebc0b3bcfc49de215cdd11b86ac1279c
madhav <madhavnepal916@gmail.com>
2025-05-14 16:49:26 +0545

    added some logic in the view

--- backend/protifilio/details/views.py
+++ backend/protifilio/details/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from  .models import HomeContent,About,Project
+from .serializer import *
+
+
+class HomeContentView(generics.ListAPIView):
+    queryset=HomeContent.objects.all()
+    serializer_class=HomeContentSerializer
+
+class AboutView(generics.ListAPIView):
+    queryset=About.objects.all()
+    serializer_class=AboutSerializer
+
+
+class ProjectView(generics.ListAPIView):
+    queryset=Project.objects.all()
+    serializer_class=ProjectSerializer
 
     
